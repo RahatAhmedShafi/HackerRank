e15d10f1b96d9e06a5668c41248949eb6951c50e
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-02-10 19:50:27 +0600

    Initial commit

---
+++ src/thirtyDaysofCode/Operators.java
@@ -0,0 +1,27 @@
+
+package thirtyDaysofCode;
+
+
+public class Operators {
+    int a=10;
+    int b=20;
+    public Operators (int aa,int bb){
+        a=aa;
+        b=bb;
+    }
+    public void car(){
+     
+        System.out.println(a);
+        System.out.println(b);
+    }
+   
+    
+    
+    
+    public static void main(String[]args){
+     Operators gg=new Operators (30,40);
+     gg.car();
+      
+    }
+    
+}
