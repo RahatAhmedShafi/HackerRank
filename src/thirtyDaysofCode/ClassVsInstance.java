407f84d1afe5f62e2146c0ff86b91cf12d4cb975
Rahat Ahmed Shafi <rahatahmed1447@gmail.com>
2023-02-11 18:38:25 +0600

    Create a class

--- src/thirtyDaysofCode/ClassVsInstance.java
+++ src/thirtyDaysofCode/ClassVsInstance.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package thirtyDaysofCode;
 
-/**
- *
- * @author ASUS
- */
-public class ClassVsInstance {
+class Person{
     
+}
+public class ClassVsInstance {
+    public static void main(String[] args) {
+        
+    }
 }
